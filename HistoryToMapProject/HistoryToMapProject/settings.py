7037e8b549366104c4ec7815d38d7913074cb65a
CoolVery <166332377+CoolVery@users.noreply.github.com>
2024-07-22 12:22:59 +0300

    Update settings.py

--- HistoryToMapProject/HistoryToMapProject/settings.py
+++ HistoryToMapProject/HistoryToMapProject/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
