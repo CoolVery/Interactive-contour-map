2e15dc0b8723c8ea8d9d418fd03329bb91e6284f
CoolVery <georgekuklevwork@gmail.com>
2024-08-07 18:28:56 +0300

    Removed images from the project, changed models. Now images are stored in the DB

--- HistoryToMapProject/WebHistory/models.py
+++ HistoryToMapProject/WebHistory/models.py
@@ -4,15 +4,16 @@ from django.db import models
 class Event(models.Model):
     id = models.AutoField(primary_key=True, blank=False)
     name_event = models.CharField(max_length=100, blank=False)
-    path_img = models.TextField(blank=False)
-    path_img_map = models.TextField(blank=False, default="test")
+    path_img = models.BinaryField(blank=False, default=b'')
+    path_img_map = models.BinaryField(blank=False, default=b'')
 
     def __str__(self):
         return self.name_event
+
 class DirectionLines(models.Model):
     id = models.AutoField(primary_key=True, blank=False)
     name_direction_line = models.TextField(blank=False)
-    path_img_line = models.TextField(blank=False)
+    path_img_line = models.BinaryField(blank=False, default=b'')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='direction_lines')
 
     def __str__(self):
@@ -20,7 +21,7 @@ class DirectionLines(models.Model):
 class SideBoundaries(models.Model):
     id = models.AutoField(primary_key=True, blank=False)
     name_side_boundaries = models.TextField(blank=False)
-    path_img_boundaries = models.TextField(blank=False)
+    path_img_boundaries = models.BinaryField(blank=False, default=b'')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='direction_boundaries')
 
     def __str__(self):
--- HistoryToMapProject/WebHistory/views.py
+++ HistoryToMapProject/WebHistory/views.py
@@ -1,21 +1,31 @@
+import base64
+
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from WebHistory.models import Event, DirectionLines, SideBoundaries
 
-
 def first_page_start(request):
     return render(request, 'firstpage.html')
 
-
 def choise_event_page(request):
     list_event = Event.objects.all().order_by('id')
+    for event in list_event:
+        event.path_img_map = f"data:image/svg+xml;base64,{bytes(event.path_img_map).decode()}"
+        event.path_img = f"data:image/svg+xml;base64,{bytes(event.path_img).decode()}"
     return render(request, 'mainpage.html', context={"list_event": list_event})
 
 def event_page(request, name_event):
     current_event = Event.objects.get(name_event=name_event.replace("_", " "))
+    event_map = f"data:image/svg+xml;base64,{bytes(current_event.path_img_map).decode()}"
     list_direction_lines = DirectionLines.objects.filter(event=current_event.id)
+    for line in list_direction_lines:
+        line.path_img_line = f"data:image/svg+xml;base64,{bytes(line.path_img_line).decode()}"
+
     list_side_boundaries = SideBoundaries.objects.filter(event=current_event.id)
+    for boundaries in list_side_boundaries:
+        boundaries.path_img_boundaries = f"data:image/svg+xml;base64,{bytes( boundaries.path_img_boundaries).decode()}"
     return render(request, 'map_page.html', context={'name_event': name_event,
                                                      'list_direction_lines': list_direction_lines,
-                                                     'event_map': current_event.path_img_map,
+                                                     'event_map': event_map,
                                                      'list_side_boundaries': list_side_boundaries})
+
