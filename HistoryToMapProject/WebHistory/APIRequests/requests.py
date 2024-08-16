e2c3dd6e69b09127fee41733fb92347f4a7c9776
CoolVery <georgekuklevwork@gmail.com>
2024-08-16 15:06:09 +0300

    Now the data is returned by the API

---
+++ HistoryToMapProject/WebHistory/APIRequests/requests.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_events():
+    url = "http://127.0.0.1:8001/event_operathion/events/"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+def get_info_events(name_event, url):
+    url = url
+    params = {
+        "name_event": name_event.replace("_", " ")
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
--- HistoryToMapProject/WebHistory/views.py
+++ HistoryToMapProject/WebHistory/views.py
@@ -2,28 +2,31 @@ import base64
 
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+from WebHistory.APIRequests.requests import get_events, get_info_events
 from WebHistory.models import Event, DirectionLines, SideBoundaries
+
 
 def first_page_start(request):
     return render(request, 'firstpage.html')
 
 def choise_event_page(request):
-    list_event = Event.objects.all().order_by('id')
+    list_event = get_events()
     for event in list_event:
-        event.path_img_map = f"data:image/svg+xml;base64,{bytes(event.path_img_map).decode()}"
-        event.path_img = f"data:image/svg+xml;base64,{bytes(event.path_img).decode()}"
+        event['path_img_map'] = f"data:image/svg+xml;base64,{event['path_img_map']}"
+        event['path_img'] = f"data:image/svg+xml;base64,{event['path_img']}"
     return render(request, 'mainpage.html', context={"list_event": list_event})
 
 def event_page(request, name_event):
-    current_event = Event.objects.get(name_event=name_event.replace("_", " "))
-    event_map = f"data:image/svg+xml;base64,{bytes(current_event.path_img_map).decode()}"
-    list_direction_lines = DirectionLines.objects.filter(event=current_event.id)
+    current_event = get_info_events(name_event, "http://127.0.0.1:8001/event_operathion/event/")
+    event_map = f"data:image/svg+xml;base64,{current_event['path_img_map']}"
+    list_direction_lines = get_info_events(name_event, "http://127.0.0.1:8001/event_operathion/line/")
     for line in list_direction_lines:
-        line.path_img_line = f"data:image/svg+xml;base64,{bytes(line.path_img_line).decode()}"
+        line['path_img_line'] = f"data:image/svg+xml;base64,{line['path_img_line']}"
 
-    list_side_boundaries = SideBoundaries.objects.filter(event=current_event.id)
+    list_side_boundaries = get_info_events(name_event, "http://127.0.0.1:8001/event_operathion/boundaries/")
     for boundaries in list_side_boundaries:
-        boundaries.path_img_boundaries = f"data:image/svg+xml;base64,{bytes( boundaries.path_img_boundaries).decode()}"
+        boundaries['path_img_boundaries'] = f"data:image/svg+xml;base64,{boundaries['path_img_boundaries']}"
     return render(request, 'map_page.html', context={'name_event': name_event,
                                                      'list_direction_lines': list_direction_lines,
                                                      'event_map': event_map,
